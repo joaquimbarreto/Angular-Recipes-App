e205c2611a93c459aab86e92b834ba360755c01c
Joaquim Barreto <joaquim@barreto.co>
2020-01-05 17:23:18 +0100

    section 2 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+// import { HttpModule } from "@angular/http";
 
 import { AppComponent } from "./app.component";
+import { ServerComponent } from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ServerComponent,
+      ServersComponent
+   ],
+   imports: [
+      BrowserModule,
+      FormsModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule {}
